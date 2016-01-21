7e62d0ee36f1428e02780ac03b0fa797b3207128
Minhas Kamal <minhaskamal024@gmail.com>
2016-01-22 00:29:54 +0600

    initial commit

--- src/CrissCrossV-1.c
+++ src/CrissCrossV-1.c
@@ -1,10 +1,30 @@
-/**
-* Game's Name: Criss_cross
-* Version: 01
-* Developer: Minhas Kamal (IIT - 5th Batch)
+/********************************************************************************************
+* Game Name: CrissCross (Version: 01)
+* Developer: Minhas Kamal (minhaskamal024@gmail.com)
 * Date: 02.June.2013
-* Comment: I wish it will give u as much pleasure as I had, developing it.
-**/
+* Brief Description: This is a simple Tic-Tac-Toe game. Players can play with the computer too.
+*     The computer has two types of AI, having two levels of smartness. I wish it will give u 
+*     as much pleasure as I had, developing it.
+*********************************************************************************************/
+
+/********************************************************************************************
+                                GNU GENERAL PUBLIC LICENSE
+                                 Version 3, 29 June 2007
+         This program is free software: you can redistribute it and/or modify
+         it under the terms of the GNU General Public License as published by
+         the Free Software Foundation, either version 3 of the License, or
+         (at your option) any later version.
+
+         This program is distributed in the hope that it will be useful,
+         but WITHOUT ANY WARRANTY; without even the implied warranty of
+         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+         GNU General Public License for more details.
+
+         You should have received a copy of the GNU General Public License
+         along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+********************************************************************************************/
+
 
 
 #include <stdio.h>
