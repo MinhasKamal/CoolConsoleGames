e92f2dd74c8e1e1159593aee740b098e3f87d8ae
Minhas Kamal <minhaskamal024@gmail.com>
2016-01-22 00:27:10 +0600

    initial commit

--- src/3Stones.c
+++ src/3Stones.c
@@ -1,10 +1,28 @@
-/**
-* Game's Name: Three_Stones
-* Version: 1
-* Developer: Minhas Kamal (IIT - 5th Batch)
+/********************************************************************************************
+* Game Name: 3Stones (Version: 1)
+* Developer: Minhas Kamal (minhaskamal024@gmail.com)
 * Date: 17.June.2013
-* Comment: I have tried to improve the game made by my friends Shamim(BSSE-0511).
-**/
+* Brief Description: In this game players place three stones on a board and move them.
+*   So that they can align them serially or place against the opponent.
+*********************************************************************************************/
+
+/********************************************************************************************
+                                GNU GENERAL PUBLIC LICENSE
+                                 Version 3, 29 June 2007
+         This program is free software: you can redistribute it and/or modify
+         it under the terms of the GNU General Public License as published by
+         the Free Software Foundation, either version 3 of the License, or
+         (at your option) any later version.
+
+         This program is distributed in the hope that it will be useful,
+         but WITHOUT ANY WARRANTY; without even the implied warranty of
+         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+         GNU General Public License for more details.
+
+         You should have received a copy of the GNU General Public License
+         along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+********************************************************************************************/
 
 
 #include <stdio.h>
